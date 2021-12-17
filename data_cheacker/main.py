3c63f9bb5559b5bf5636cd149b6af140b2607244
Hikotomo! <66200485+tomohiko9090@users.noreply.github.com>
2021-12-17 16:51:47 +0900

    Update main.py

--- data_cheacker/main.py
+++ data_cheacker/main.py
@@ -2,6 +2,6 @@ def data_cheacker(data):
   print("特徴的なサンプルサイズ")
   print("---------------------------")
   print(f"0: {list(data).count(0)}個")
-  print(f"nan: {list(data).count(np.nan)}個")
+  print(f"nan: {Series(data).isnull().sum()}個")
   print(f"inf: {list(data).count(np.inf)}個")
-  print("---------------------------")
+  print("---------------------------")  
