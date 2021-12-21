77f49ed229798775f34c5d5a0acee8c03a819b74
Hikotomo! <66200485+tomohiko9090@users.noreply.github.com>
2021-12-21 16:36:53 +0900

    Update main.py

--- data_cheacker/main.py
+++ data_cheacker/main.py
@@ -1,7 +1,12 @@
-def data_cheacker(data):
-  print("特徴的なサンプルサイズ")
+def data_counter(data):
+  '''
+  input: list or Series data
+  output: sample size
+  '''
+  print(f"total sample size: {len(user_interval_median_dict)}")
   print("---------------------------")
-  print(f"0: {list(data).count(0)}個")
-  print(f"nan: {Series(data).isnull().sum()}個")
-  print(f"inf: {list(data).count(np.inf)}個")
-  print("---------------------------")  
+  print(f"absolute over「0」: {len([i for i in data if 0 < abs(i)])}")
+  print(f"「0」: {list(data).count(0)}")
+  print(f"「nan」: {Series(data).isnull().sum()}")
+  print(f"「inf」: {list(data).count(np.inf)}")
+  print("---------------------------") 
