87e443ac1a46d1d7d97298b11e444e9ca4a08a27
Hikotomo! <66200485+tomohiko9090@users.noreply.github.com>
2021-12-23 13:33:52 +0900

    Update main.py

--- data_cheacker/main.py
+++ data_cheacker/main.py
@@ -4,9 +4,10 @@ def data_counter(data):
   output: sample size
   '''
   print("---------------------------")
-  print(f"over absolute「0」: {len([i for i in data if 0 < abs(i)])}")
+  print(f"「1」: {list(data).count(1)}")
   print(f"「0」: {list(data).count(0)}")
   print(f"「nan」: {Series(data).isnull().sum()}")
   print(f"「inf」: {list(data).count(np.inf)}")
+  print(f"over absolute「0」: {len([i for i in data if 0 < abs(i)])}")
   print("---------------------------")  
   print(f"total sample size: {len(data)}")
